3c68afc28e21de70c651a89d98a7106aff1e2a17
Yuv <billiondollarone07@gmail.com>
2018-10-04 20:14:41 +0530

    aa

--- form.php
+++ form.php
@@ -1,12 +1,14 @@
 <?php
 if(!isset($_POST['submit']))
 {
+	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
 }
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
+//Validate first
 if(empty($name)||empty($visitor_email)) 
 {
     echo "Name and email are mandatory!";
@@ -19,18 +21,21 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = "$visitor_email";
-$email_subject = "New Remark!";
+$email_from = 'billiondollarone07@gmail.com';//<== update the email address
+$email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
     
-$to = "thrivemag@tagoreint.com";
+$to = "thrivemag@tagoreint.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
 mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
 header('Location: thank-you.html');
 
 
+// Function to validate against any email injection attempts
 function IsInjected($str)
 {
   $injections = array('(\n+)',
